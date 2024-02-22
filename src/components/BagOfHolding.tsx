b84cf60fc178d7900dc7352bbd4986d7c06ff257
prabhjot98 <pssandhu@ucdavis.edu>
2024-02-21 20:20:16 -0800

    chore: remove unused component

--- src/components/BagOfHolding.tsx
+++
@@ -1,6 +0,0 @@
-import { type JSXElement } from 'solid-js'
-import { type Inventory } from '../types/inventory'
-
-export const BagOfHolding = (props: { inventory: Inventory }): JSXElement => {
-  return <div>Bag of holding goes here{}</div>
-}
